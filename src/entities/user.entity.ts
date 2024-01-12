da0f3a8842caed4369d15a3f97598739eb3335a5
alessandro <git.aleneves@gmail.com>
2024-01-12 05:22:58 -0300

    Refactor: improves user entity and rm others

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,12 +1,16 @@
+import { Role, Situation } from "@prisma/client"
 import { Exclude } from "class-transformer"
 import { IsEmail, MinLength } from "class-validator"
-import { Permission } from "./permission.entity"
+
 
 export class User {
   id: number
   
-  @MinLength(2)
+  @MinLength(4)
   name: string
+
+  @MinLength(4)
+  username: string
 
   @IsEmail()
   email: string
@@ -15,12 +19,13 @@ export class User {
   @MinLength(8)
   password: string
 
-  isAdmin: boolean
+  @Exclude()
+  role: Role
 
-  permissions: Permission[]
+  situation: Situation
 
   constructor(data: User) {
-    const { id, name, email, password, isAdmin, permissions } = data;
-    Object.assign(this, {id, name, email, password, isAdmin, permissions});
+    const { id, name, username, email, password, role, situation } = data;
+    Object.assign(this, {id, name, username, email, password, role, situation});
   }
 }
